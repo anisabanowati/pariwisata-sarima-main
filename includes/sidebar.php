1a0d476c3d9a3ad9a976dea8f7283e308937da4a
Ramadhaniyanto19 <mramadhaniyanto@gmail.com>
2025-07-10 22:47:26 +0700

    fix role sidebar

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -1,4 +1,4 @@
- <nav class="sidebar sidebar-offcanvas" id="sidebar">
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
     <ul class="nav">
         <li class="nav-item">
             <a class="nav-link" href="dashboard.php">
@@ -7,19 +7,19 @@
             </a>
         </li>
 
-        <!-- <li class="nav-item">
-            <a class="nav-link" href="manage_pengelola.php">
-                <span class="menu-title">Manage Pengelola</span>
-                <i class="mdi mdi-account menu-icon"></i>
-            </a>
-        </li> -->
+        <?php
+        // Get user role from session (assuming it's stored during login)
+        $userRole = $_SESSION['role'] ?? 'user'; // Default to 'user' if not set
 
-        <li class="nav-item">
-            <a class="nav-link" href="new_visitor.php">
-                <span class="menu-title">Input Data Wisata</span>
-                <i class="mdi mdi-account menu-icon"></i>
-            </a>
-        </li>
+        // Show "Input Data Wisata" only for admin
+        if ($userRole !== 'Admin'): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="new_visitor.php">
+                    <span class="menu-title">Input Data Wisata</span>
+                    <i class="mdi mdi-account menu-icon"></i>
+                </a>
+            </li>
+        <?php endif; ?>
 
         <li class="nav-item">
             <a class="nav-link" href="hasil_predik.php">
@@ -34,39 +34,16 @@
                 <i class="mdi mdi-account-multiple menu-icon"></i>
             </a>
         </li>
-        <?php
-        $aid=$_SESSION['odmsaid'];
-        $sql="SELECT * from  tbladmin where ID=:aid";
-        $query = $dbh -> prepare($sql);
-        $query->bindParam(':aid',$aid,PDO::PARAM_STR);
-        $query->execute();
-        $results=$query->fetchAll(PDO::FETCH_OBJ);
-        $cnt=1;
-        if($query->rowCount() > 0)
-        {  
-            foreach($results as $row)
-            { 
-                if($row->AdminName=="Admin"  )
-                { 
-                    ?>
-                    <li class="nav-item">
-                        <a class="nav-link" data-toggle="collapse" href="#general-pages" aria-expanded="false" aria-controls="general-pages">
-                            <span class="menu-title" href="userregister.php">Management Wisata</span>
-                            <i class="menu-arrow"></i>
-                            <i class="mdi mdi-account-multiple menu-icon"></i>
-                        </a>
-                        <div class="collapse" id="general-pages">
 
-                            <ul class="nav flex-column sub-menu">
-                                <li class="nav-item"> <a class="nav-link" href="userregister.php">Register Wisata </a></li>
-                                <li class="nav-item"> <a class="nav-link" href="user_permission.php"> User permissions</a></li>
-                            </ul>
-                        </div>
-                    </li>
-                    <?php 
-                } 
-            }
-        } ?> 
-        
- </ul>
+        <?php
+        // Show "Management User" only for admin
+        if ($userRole !== 'User'): ?>
+            <li class="nav-item">
+                <a class="nav-link" href="userregister.php">
+                    <span class="menu-title">Management User</span>
+                    <i class="mdi mdi-account-multiple menu-icon"></i>
+                </a>
+            </li>
+        <?php endif; ?>
+    </ul>
 </nav>
